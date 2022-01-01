6c0dc51e15adaae477062c1969ce087dd9371500
ahmadhafis <ahmadhafis296@gmail.com>
2022-01-01 18:38:10 +0700

    Edit untuk route auth dan controller auth

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -19,5 +20,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('books', BookController::class);
+Route::middleware('auth:sanctum')->group( function () {
+    Route::apiResource('books', BookController::class);
+});
+
+Route::post('auth/login', [AuthController::class, 'login']);
+Route::post('auth/register', [AuthController::class, 'register']);
 
