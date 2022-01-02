324f9321fe35e151783b01c3a299a5479c639227
nailanabilaaaaaa <nailanabilaaaaaa@gmail.com>
2022-01-02 13:10:45 +0700

    Update Api Route

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\BookController;
+use App\Http\Controllers\Api\ChapterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,6 +23,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::middleware('auth:sanctum')->group( function () {
     Route::apiResource('books', BookController::class);
+    Route::apiResource('chapters', ChapterController::class);
 });
 
 Route::post('auth/login', [AuthController::class, 'login']);
