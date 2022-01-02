3c447ebbd3e0d13c7cd5136b0816ae9edf3ddf22
ahmadhafis <ahmadhafis296@gmail.com>
2022-01-02 08:33:04 +0700

    add logout route

--- routes/api.php
+++ routes/api.php
@@ -26,4 +26,4 @@ Route::middleware('auth:sanctum')->group( function () {
 
 Route::post('auth/login', [AuthController::class, 'login']);
 Route::post('auth/register', [AuthController::class, 'register']);
-
+Route::post('auth/logout', [AuthController::class, 'logout']);
